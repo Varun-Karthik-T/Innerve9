5b6644ec9b353ef8c3fb8aad3b1f449a95949fcf
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2025-01-19 00:22:24 +0530

    updated addIssue.js

--- server/app.js
+++ server/app.js
@@ -3,9 +3,6 @@ const app = express();
 const bodyParser = require("body-parser");
 const connectDB = require('./config/db');
 const cors = require("cors");
-const Issue = require("./models/Issue");
-const Counter = require("./models/Counter"); 
-const IssueChain = require("./tests/issueFunctions");
 
 const getTender = require("./routes/tenders"); 
 const getContract = require("./routes/contracts");
@@ -16,6 +13,7 @@ const getContractById = require("./routes/getContractById");
 const getIssue = require("./routes/getIssues");
 const getVoteStatus = require("./routes/get-voteStatus");
 const vote = require("./routes/vote");
+const addIssue = require("./routes/addIssue");
 
 const PORT = process.env.PORT || 4000; 
 const HOST = "0.0.0.0";
@@ -32,51 +30,10 @@ app.use("/addContract", addContract);
 app.use("/updateContract", updateContract);
 app.use("/", getVotes);
 app.use("/", getContractById);
-app.use("/issues", getIssue);
+app.use("/issues", getIssue);       //getIssue
 app.use("/", getVoteStatus);
-app.use("/vote", vote);
-
-app.post("/issues", async (req, res) => {
-  try {
-    const issue = req.body;
-    console.log("POST /issues", issue);
-
-    const counter = await Counter.findByIdAndUpdate(
-      { _id: "issueId" },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
-    );
-
-    issue.id = counter.seq;
-
-    const newIssue = new Issue(issue);
-    await newIssue.save();
-    
-await IssueChain.createIssue({
-    issueId: issue.id,
-    issueName: issue.issue_type,
-    description: issue.description,
-    dateOfComplaint: issue.date_of_complaint,
-    approval: issue.approval,
-    denial: issue.denial,
-    accuracy: Math.floor(80),
-    altitude: Math.floor(0),
-    latitude: Math.floor(issue.location.latitude * 1000000),
-    longitude: Math.floor(issue.location.longitude * 1000000)
-}, process.env.ACCOUNT_ADDRESS).then((res) => {
-    console.log("Issue added to chain successfully");
-}).catch((error) => {
-    console.log("Error adding issue in ConTrack Network:", error);
-    return res.status(500).json({ error: error.message });
-});
-    return res
-      .status(201)
-      .json({ message: "Issue created successfully", issue: newIssue });
-  } catch (error) {
-    console.error("Error creating issue:", error);
-    res.status(500).json({ error: error.message });
-  }
-});
+app.use("/vote", vote);             //addVote
+app.use("/issues", addIssue);       //addIssue 
  
 app.use("/*", (req, res) => {
   res.status(404).json({ message: "Endpoint not found" });
